51aa35c3e0c4552efbfde8f0916553fbe2e17971
Matthew Naul <mnaul@armt.com>
2017-04-17 13:37:10 -0500

    dosen't help systems where the extention is not loaded.

--- query_stats/queries.sql
+++ query_stats/queries.sql
@@ -3,6 +3,4 @@ SELECT LEFT(query,50) AS query,
        calls, total_time, rows, shared_blks_hit,
        local_blks_hit, blk_read_time, blk_write_time
 FROM pg_stat_statements
-WHERE EXISTS(SELECT * FROM pg_available_extensions
-             WHERE name = 'pg_stat_statements')
 ORDER BY calls DESC;
